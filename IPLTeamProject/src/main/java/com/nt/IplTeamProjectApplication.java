3b31a1d9dea91ff8ece13c200df2190f92ba3216
TL <ram@gmail.com>
2025-12-10 16:17:10 +0530

    commit the tl

---
+++ IPLTeamProject/src/main/java/com/nt/IplTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplTeamProjectApplication.class, args);
+	}
+
+}
